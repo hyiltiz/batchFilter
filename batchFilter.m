06caf60fcd5081ebb33829b6f040486321192e18
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-05-24 21:22:55 +0800

    document indexVar and groupVar

--- batchFilter.m
+++ batchFilter.m
@@ -1,4 +1,4 @@
-function stat = batchFilter(matfiles, varList, indexList, batchFun, outFiles, options)
+function stat = batchFilter(matfiles, varList, indexList, groupVar, batchFun, outFiles, options)
 %BATCHFILTER applies batchFun as a filter to multiple mat-files then combines output into statistics-friendly tables.
 %
 % SYNOPSIS: stat = batchFilter(matfiles, varList, indexList, batchFun, outFiles, options)
@@ -7,7 +7,7 @@ function stat = batchFilter(matfiles, varList, indexList, batchFun, outFiles, op
 % matFiles
 %       matFiles specifies the pathname of the data files to be opened as mat-files.
 %       The pathname could either be relative pathname to the current directory,
-%       or absolute pathname. When empty, batchFilter searches for mat-files with
+%       or absolute pathname. When empty, batchFilter searches by defaultfor mat-files with
 %       .mat suffix first under an ./data directory (if one exists under current
 %       directory), then under current directory.
 %       The data files could be specified with one of the following synatx:
@@ -31,7 +31,50 @@ function stat = batchFilter(matfiles, varList, indexList, batchFun, outFiles, op
 %            single mat-file to be treated as the data-file to be processed.
 %            For expmple: {'data/a.mat', 'data/b.mat'} processes all data files as mat-files.
 % 	varList
+        A cell string of size 1 x n or a single string that specifies n variables
+        or s single variable in every data files to be processed. The string could be a literal string specifying
+        the variable name, a glob pattern consisting one or more asterisks, or
+        a regular expression. Use '1' to specify ALL the variables and 'eye()' or 'file()'
+        to specify the variable which has the same name with the data file it was
+        stored in.
+        Use a cell string of size m x n to respectively specify the variabbles
+        to be processed for every single data files. m should be the same with
+        the number of data files to be processed.
+        For example: 'Trials' % to process variable `Trials` in all the data files
+                    {'Pre', 'Post'} % to process variables `Pre` and `Post` in all the data files
+                    {'train*'} % or 'train*' to process variables `train1`, `train2` etc. in all the data files
+                    {'1'; 'eye()'} % process ALL the variables in the first mat file, and process the variable
+                    that shares name with the second data file
 %		indexList
+        A single integer or an 1 x n array of integers that specifies the COLUMN indices
+        of each variables to be processed. Use m x n array of integers to respectively
+        specify the COLUMNS indices for each of the variables to be processed.
+        Could also be a string that satisfies one of the folowing cases:
+          1. 'T(rowIndex, n)'. This transposes the variable, and then uses the row
+             index as the column index for the transposed variable.
+          2. 'P(dimIndex, ORDER)'. This permutes the variable using `permute(x, order)`, and then uses dimIndex
+              as the column index (second dimention) for the transposed variable. NOT IMPLEMENTED YET.
+          3. A valid index which could be literally used as MATLAB index, e.g.
+             '1:end', '3:6', '[2 4 end-1]' etc.
+          4. 'eye()' specifies ALL the columns. This is equivalent to '1:end'.
+          5. 'file()' chooses the variable that shares the same name with the data
+             file to be used.
+          6. 'indexVarName' specifies the variable `indexVarName` in the data
+            file whose values are to be treated as the index array.
+       Use cell strings of size m x 1 to specify respectively for each variable.
+       For example:
+%   groupVar
+        A single integer or an 1 x n array of integers specifying indices of columns which serves as group variables for values specified in the COLUMN indices
+        of each variables to be processed. The indexed (indexList) column(s) in each variable (varList) will be
+        grouped based on the values of the column(s) specified by groupVar of that variable (varList).
+        Since grouVar is also indices, it is specified similiarly
+        as indexList. In addition, groupVar could also be a single string specifying
+        a variable in the data file to be used as the group variable (in
+        comparison with specifying one or several column indices of the same
+        variable to be processed). All the columns of this group variable will
+        be used for grouping.
+        Use a cell string of size m x 1 to specify seperate group variables for
+        each data file respectively.
 %		batchFun
 %		outFiles
 %   options
