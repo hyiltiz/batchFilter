63bafb70912213ef7e148b4242d331c238bafcf3
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-05-24 19:44:53 +0800

    mat file selection as a module

--- batchFilter.m
+++ batchFilter.m
@@ -73,76 +73,16 @@ options.defaultRegexStartDir = '.';
 % select the mat files
 % NOTE: maybe simply using the path mechanism would be easier, but it introduces
 % potential bugs where unintended mat-files get processed.
-checkNextDir = 0;
-suffix = options.defaultSuffix{1};
-regexStartDir = options.defaultRegexStartDir;
-if exist('maFiles', 'var') ~= 1
-  % You really should have provided this! Be explicit!
-  
-  for iDir = 1:options.defaultDirs
-    thisDir = options.defaultDirs{iDir};
-    if exist(thisDir, 'dir') ~= 7
-      if isempty(ls([thisDir] '*' suffix]))
-        % Found no mat-file under data. Go search for current directory
-        checkNextDir = 1;
-      else
-        warning('batchFilter:matFiles', ['Auto-selecting all mat files under `' thisDir '`']);
-        matFiles = thisDir;
-      end
-    else
-      checkNextDir = 1;
-    end
-  end
-  
-  if ischar(matFiles) % char: could be 1) dir, 2) glob, 3) regex
-    % decide is this is regexp
-    if ~isempty(regexp(matFiles, '^s/.*/$'))
-      % this could be regexp
-      if exist(matFiles, 'dir') == 7
-        % this could also be valid directory
-        warning('batchFilter:matFiles', ['Auto-selecting all mat files under `' matFiles '`\n'...
-          'Or did you mean a REGular EXpression by that!?'...
-          'If so, please rename that directory first.']);
-      else
-        % this is a regexp
-        matFiles = matFiles(3:end-1); % strip out s//
-        allFiles = getAllFiles(regexStartDir, matFiles, options.isMatchFullPath);
-        
-        if options.isMatchFullPath
-          matchstart = regexp(allFiles, matFiles);
-          matFiles = allFiles(~cellfun(@isempty, matchstart));
-        end
-      end
-      
-    elseif ~isempty(regexp(matFiles, '\*'))
-      % found a glob pattern
-      matFiles = cellstr(ls(matFiles));
-    else
-      % could only be a dir
-      matFiles = [matFiles '/']; % append an /
-      matFiles = regexprep(matFiles, '\\', '/'); % always use unix style path
-      matFiles = regexprep(matFiles, '/*$', '/');
-    end
-  end
-  
-  
-  % parse them into the list format
-  if iscellstr(matFiles)
-    % good!
-    matFilesList = matFiles; % treat AS IS
-  elseif exist(matFiles, 'dir') == 7
-    matFilesList = cellstr(ls([matFiles '*.mat']));
-  elseif exist(matFiles, 'file') == 2
-    matFilesList = {matFiles}; % this is a single mat file
-  end
-  nMatFiles = numel(matFilesList);
-  
-  
-  
-  
+
+[matFilesList status.selectMatFiles] = selectMatFiles(matFiles, options.defaultSuffix{1}, options.defaultRegexStartDir);
+nMatFiles = numel(matFilesList);
+
+
+
+
   getCenter = statFun('mean', 1); % or choose median
   getDispersion = statFun('std', 1); % or choose mad/irq
-  
+
   data = [];
   for iSub = 1:numel(files)
     s = load([dir files{iSub}]);
@@ -151,5 +91,5 @@ if exist('maFiles', 'var') ~= 1
     [typeAV, typeRegIrreg, avgInterval] = ind2sub(size(mC{iSub}), find(mC{iSub}));
     data = [data; [iSub*ones(size(typeAV)), typeAV, typeRegIrreg, avgInterval, mC{iSub}(:), sdC{iSub}(:)]];
   end
-  
+
 end
---
+++ selectMatFiles.m
@@ -0,0 +1,103 @@
+function [matFilesList status] = selectMatFiles(matFiles, suffix, regexStartDir)
+% select the mat files
+% matFiles
+%       matFiles specifies the pathname of the data files to be opened as mat-files.
+%       The pathname could either be relative pathname to the current directory,
+%       or absolute pathname. When empty, batchFilter searches for mat-files with
+%       .mat suffix first under an ./data directory (if one exists under current
+%       directory), then under current directory.
+%       The data files could be specified with one of the following synatx:
+%         1. A directory string. In this case, all mat-files under this directory is
+%            processed.
+%            For example: 'exp1/data/' searches under exp1/data/
+%         2. A glob pattern string consisting one or more asterisks (*) sign which represents zero or more of any charachters. The glob
+%            pattern will be expanded by MATLAB's native ls command, which in turn,
+%            relies upon the `ls` or `dir` utilities in the operating system.
+%            For example: 'exp*/data/' searches through data under exp1, exp2 etc.
+%         3. A regular expressions patteen (regexp) string inside 's/<pattern>/'.
+%          If a regexp is provided, the pattern is matched against the relative full path under
+%            current directory. Setting options.isMatchFullPath to 0 can change
+%            this matching mode so that the pattern is matched against each file
+%            (e.g. 'a.mat') and folder names (e.g. 'exp1', 'data') instead of
+%            relative full path names (e.g. './exp1/data/a.mat'). The regexp matching
+%            is procecssed by MATLAB's native `regexp` command. see DOC REGEXP
+%            for more information.
+%            For example, 's/.*john.*\.mat/' searches for all .mat files whose name contains 'john'
+%         4. A list of cell strings where each elements of the cell string is a
+%            single mat-file to be treated as the data-file to be processed.
+%            For expmple: {'data/a.mat', 'data/b.mat'} processes all data files as mat-files.
+
+if exist('suffix', 'var') ~= 1
+  suffix = '.mat';
+end
+
+if exist('regexStartDir', 'var') ~= 1
+  regexStartDir = '.';
+end
+
+checkNextDir = 0;
+if exist('maFiles', 'var') ~= 1
+  % You really should have provided this! Be explicit!
+  
+  for iDir = 1:options.defaultDirs
+    thisDir = options.defaultDirs{iDir};
+    if exist(thisDir, 'dir') ~= 7
+      if isempty(ls([thisDir '*' suffix]))
+        % Found no mat-file under data. Go search for current directory
+        checkNextDir = 1;
+      else
+        warning('batchFilter:matFiles', ['Auto-selecting all mat files under `' thisDir '`']);
+        matFiles = thisDir;
+      end
+    else
+      checkNextDir = 1;
+    end
+  end
+  
+  if ischar(matFiles) % char: could be 1) dir, 2) glob, 3) regex
+    % decide is this is regexp
+    if ~isempty(regexp(matFiles, '^s/.*/$', 'once'))
+      % this could be regexp
+      if exist(matFiles, 'dir') == 7
+        % this could also be valid directory
+        warning('batchFilter:matFiles', ['Auto-selecting all mat files under `' matFiles '`\n'...
+          'Or did you mean a REGular EXpression by that!?'...
+          'If so, please rename that directory first.']);
+      else
+        % this is a regexp
+        status.isRegexp = 1;
+        matFiles = matFiles(3:end-1); % strip out s//
+        allFiles = getAllFiles(regexStartDir, matFiles, options.isMatchFullPath);
+        
+        if options.isMatchFullPath
+          matchstart = regexp(allFiles, matFiles);
+          matFiles = allFiles(~cellfun(@isempty, matchstart));
+        end
+      end
+      
+    elseif ~isempty(regexp(matFiles, '\*', 'once'))
+      % found a glob pattern
+      status.isGlob = 1;
+      matFiles = cellstr(ls(matFiles));
+    else
+      % could only be a dir
+      status.isDir = 1;
+      matFiles = [matFiles '/']; % append an /
+      matFiles = regexprep(matFiles, '\\', '/'); % always use unix style path
+      matFiles = regexprep(matFiles, '/*$', '/');
+    end
+  end
+  
+  
+  % parse them into the list format
+  if iscellstr(matFiles)
+    % good!
+    matFilesList = matFiles; % treat AS IS
+  elseif exist(matFiles, 'dir') == 7
+    matFilesList = cellstr(ls([matFiles '*' suffix]));
+  elseif exist(matFiles, 'file') == 2
+    status.isSingleFile = 1;
+    matFilesList = {matFiles}; % this is a single mat file
+  end
+end
+end
