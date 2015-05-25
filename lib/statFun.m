6a472f947989607cc17255fe5e8bffbb16861ca7
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-05-25 13:29:51 +0800

    statFun registration

--- lib/statFun.m
+++ lib/statFun.m
@@ -1,20 +1,42 @@
 function handle = statFun(funType, dim, args)
 % use function(x, dim) for all statistics functions
 
-switch lower(funType)
-    case {'mean', 'nanmean'}
-        handle = @(x) nanmean(x, dim);
-    case {'trimmean', 'trimmedmean'}
-        if ~exist('args', 'var')
-            percent = 10; % percent
-        else
-            percent = args;
-        end
-        handle = @(x) trimmean(x, percent, 'round', dim);
-    case {'median', 'nanmedian'}
-        handle = @(x) nanmedian(x, dim);
-    case {'std', 'nanstd'}
-        handle = @(x) nanstd(x, 1, dim);
+% register valid functions that are to be defined below
+statFunctions = struct(...
+  'mean', {{'mean', 'nanmean'}},...
+  'trimmean',  {{'trimmean', 'trimmedmean'}},...
+  'median', {{'median', 'nanmedian'}},...
+  'std', {{'std', 'nanstd'}}...
+  );
+
+if exist('dim', 'var') ~= 1
+  dim = 1; % default to column-wise operation
+end
+
+
+if nargin == 0
+  % display all possible cases
+  disp(fieldnames(statFunctions));
+  
+else
+  
+  switch lower(funType)
+    case statFunctions.mean
+      handle = @(x) nanmean(x, dim);
+    case statFunctions.trimmean
+      if ~exist('args', 'var')
+        percent = 10; % percent
+      else
+        percent = args;
+      end
+      handle = @(x) trimmean(x, percent, 'round', dim);
+    case statFunctions.median
+      handle = @(x) nanmedian(x, dim);
+    case statFunctions.std
+      handle = @(x) nanstd(x, 1, dim);
     otherwise
-        error('statFun:undefined', 'Undefined statistics function!');
+      error('statFun:undefined', 'Undefined statistics function!');
+  end % switch
+end
+
 end
