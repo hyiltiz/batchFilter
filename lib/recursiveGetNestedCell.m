b2c9b0889e20f8ad512cd527401e8906c8bba33d
Hormetjan Yiltiz <hyiltiz@gmail.com>
2015-06-03 19:37:04 +0800

    evalin('caller',... is what we wanted, not eval

--- lib/recursiveGetNestedCell.m
+++ lib/recursiveGetNestedCell.m
@@ -1,14 +1,17 @@
 function [x, xStr] = recursiveGetNestedCell(str)
 % return the nested cell content of a cell
-% str is of format '{1}{3}'
+% str is of format 'x{1}{3}'
 
-if iscell(str) % the value could be cell, but not this one
+varPart = regexprep(str, '^([a-zA-Z]+\w*)\{.*', '$1');
+var = evalin('caller', varPart);
+
+if iscell(var) % the value could be cell, but not this one
 % >> regexprep('{1}{3}(1:end)', '^(\{.*\})*(\(.*\))?$', '$1')
 % ans =
 % {1}{3}
-xStr = regexprep(indexStr, '^(\{.*\})*(\(.*\))?$', '$1');
+xStr = regexprep(str, '^(\{.*\})*(\(.*\))?$', '$1');
 
-x = eval(xStr);
+x = evalin('caller', xStr);
 else
 warning('recursiveGetNestedCell:notCellInput',...
 'The indexed variable is not a valid cell.');
